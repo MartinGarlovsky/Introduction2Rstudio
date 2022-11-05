057fc74624c534a4e2acaa41a9579e423a6a9aaf
MartinGarlovsky <chuzelwit@hotmail.com>
2022-11-05 15:58:37 +0100

    wflow_publish("dir_script.R")

--- dir_script.R
+++ dir_script.R
@@ -1,10 +1,10 @@
-# # create subfolders for directory subscturcture
-# base_path <- getwd()
-# subfolder_names <- c("scripts", "data", "figures", "output")
-#
-# for (name in subfolder_names) {
-#
-#   path <- paste0(base_path, "/", name)
-#   dir.create(path)
-#
-# }
+# create subfolders for directory subscturcture
+base_path <- getwd()
+subfolder_names <- c("scripts", "data", "figures", "output")
+
+for (name in subfolder_names) {
+
+  path <- paste0(base_path, "/", name)
+  dir.create(path)
+
+}
